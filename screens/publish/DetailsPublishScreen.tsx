29ffba9ed6b4de5df725fa73a1f9a44efe7a9399
Ronfflex <aiglevisnky@gmail.com>
2024-02-04 14:39:00 +0100

    feat(detailsPublish): adding datepicker and nbpassengers picker

--- screens/publish/DetailsPublishScreen.tsx
+++ screens/publish/DetailsPublishScreen.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Platform } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../App";
 import Icon from "react-native-vector-icons/FontAwesome";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-picker/picker";
 
 type DetailsPublishScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -23,21 +24,23 @@ type Props = {
 const DetailsPublishScreen = ({ route, navigation }: Props) => {
   const { depart, arrival } = route.params;
   console.log(depart, arrival);
-  const [selectedDate, setSelectedDate] = useState("");
-  const [selectedNbPassengers, setSelectedNbPassengers] = useState("");
-  const [date, setDate] = useState(new Date());
-  
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedNbPassengers, setSelectedNbPassengers] = useState("1");
+  console.log(selectedDate, selectedNbPassengers);
+
   const [inputIconsColor, setInputIconsColor] = useState("lightslategrey");
 
   const onChangeDate = (event: any, selectedDate: any) => {
     if (event.type !== "dismissed") {
-      const currentDate = selectedDate || date;
-      setDate(currentDate);
+      const currentDate = selectedDate || setSelectedDate;
+      setSelectedDate(currentDate);
     }
   };
 
   const handleContinue = () => {
     // TODO
+    // -> Send the data to the DB
+    // -> Redirect to another page (confirmation page, home page, ... idk yet)
   };
 
   return (
@@ -48,40 +51,50 @@ const DetailsPublishScreen = ({ route, navigation }: Props) => {
         </Text>
       </View>
 
-      <View className="flex-row mt-5 mx-5">
+      <View className="flex-1 mt-5 mx-5">
         {/* Date */}
         <View
-          className="w-3/5 flex-row mr-1 justify-center shadow border-2 border-2 border-zinc-800 rounded-2xl h-16"
+          className="justify-center shadow border-2 border-2 border-zinc-800 rounded-2xl h-16 mb-14"
           style={{ backgroundColor: "#1c1c1c" }}
         >
-          {/* Icon */}
           <View className="flex-row items-center pl-5 pr-2">
             <Icon name={"calendar"} size={20} color={inputIconsColor} />
             <DateTimePicker
-              value={date}
-              mode="date"
+              value={selectedDate}
+              mode="datetime"
               display="default"
               onChange={onChangeDate}
               themeVariant="dark"
             />
           </View>
-
-          {/* Input */}
-          <View className="flex-none justify-center pl-5 pr-2"></View>
         </View>
 
         {/* Number of passengers */}
         <View
-          className="w-2/5 flex-row ml-1 justify-center shadow border-2 border-2 border-zinc-800 rounded-2xl h-16"
+          className="justify-center shadow border-2 border-2 border-zinc-800 rounded-2xl h-16"
           style={{ backgroundColor: "#1c1c1c" }}
         >
-          <View className="flex-none justify-center pl-5 pr-2">
-            <Text className="text-white text-xl font-bold">👥</Text>
+          <View className="flex-row items-center px-5">
+            <Icon name={"users"} size={20} color={inputIconsColor} />
+            <Picker
+              style={{ width: "100%", color: "lightslategrey" }}
+              selectedValue={selectedNbPassengers}
+              onValueChange={(itemValue) => setSelectedNbPassengers(itemValue)}
+            >
+              {[...Array(10).keys()].map((number) => (
+                <Picker.Item
+                  color="white"
+                  key={number}
+                  label={`${number + 1}`}
+                  value={`${number + 1}`}
+                />
+              ))}
+            </Picker>
           </View>
         </View>
       </View>
 
-      {/* Input */}
+      {/* Next page */}
       <View className="flex-none grow px-2">
         <View className="flex-1 justify-center gap-y-0.5 items-start">
           <TouchableOpacity
